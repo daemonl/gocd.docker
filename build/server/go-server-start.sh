5a91c59c3f030db397ed353de9ec6bcd8a6bbe20
Damien Whitten <damien@rebase.com.au>
2016-04-20 17:31:41 +1000

    Tweak

--- build/server/go-server-start.sh
+++ build/server/go-server-start.sh
@@ -9,6 +9,5 @@ AGENT_KEY="${AGENT_KEY:-123456789abcdef}"
 [[ -n "$AGENT_KEY" ]] && sed -i -e 's/agentAutoRegisterKey="[^"]*" *//' -e 's#\(<server\)\(.*artifactsdir.*\)#\1 agentAutoRegisterKey="'$AGENT_KEY'"\2#' /etc/go/cruise-config.xml
 
 chown -R go:go /var/lib/go-server /var/log/go-server /etc/go /go-addons /var/go
-cp log4j.properties /etc/go/
 
 /bin/su - go -c /usr/share/go-server/server.sh
